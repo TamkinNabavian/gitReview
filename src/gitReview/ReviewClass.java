fdf2132d93b53a573ef6f13e857108164bafeeae
Tamkin <tamkin.nabavian@tekschool.us>
2022-11-09 20:15:37 -0500

    3nd commit

--- src/gitReview/ReviewClass.java
+++ src/gitReview/ReviewClass.java
@@ -3,7 +3,11 @@ package gitReview;
 public class ReviewClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		public void test() {
+			
+		System.out.println("test");
 
 	}
 
